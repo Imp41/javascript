9d792abb9f61f78ecfe0487a8957d9ce10ff1f1b
Imp41 <asmartins177@gmail.com>
2023-04-26 17:56:03 +0100

    If..

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu automovel é ${vel} Km/h`)
+if (vel > 60) { // Condição Simples! 
+    console.log(`Você ultrapassou o limite de velocidade permitida. MULTADO!`)
+}
+console.log(`Conduza sempre usando o cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Vive em ${país}`)
+if (país == 'Portugal') {
+    console.log('Você é Português!') 
+    }
+else {
+    console.log('Você é estrangeiro!')
+}
