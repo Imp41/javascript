7b3a1806f216331c3e1bbad0ad54721b57d076f2
Imp41 <asmartins177@gmail.com>
2023-05-24 19:26:18 +0100

    teste calculadora

---
+++ testes/calculadora/calculadora.js
@@ -0,0 +1,20 @@
+let displayElement = document.getElementById('display')
+let number1Button = document.getElementById('number1')
+let number2Button = document.getElementById('number2')
+let number3Button = document.getElementById('number3')
+let number4Button = document.getElementById('number4')
+let number5Button = document.getElementById('number5')
+let number6Button = document.getElementById('number6')
+let number7Button = document.getElementById('number7')
+let number8Button = document.getElementById('number8')
+let number9Button = document.getElementById('number9')
+let number0Button = document.getElementById('number0')
+let somarButton = document.getElementById('somar')
+let subtrairButton = document.getElementById('subtrair')
+let multiplicarButton = document.getElementById('multiplicar')
+let dividirButton = document.getElementById('dividir')
+let igualButton = document.getElementById('igual')
+
+function insert(num) {
+    document.getElementById
+}
---
+++ testes/teste01.js
@@ -0,0 +1,6 @@
+var hora = 0
+
+while (hora <= 8) {
+    console.log(`São ${hora} horas. Acordo às 9`)
+    hora++
+}
